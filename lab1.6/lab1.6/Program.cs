9393439d39c4d520c9d6b6b401c6717e01d1a1bd
BetejkoK <91145151+BetejkoK@users.noreply.github.com>
2021-09-21 19:11:28 +0300

    lab 1.1, 1.6

---
+++ lab1.6/lab1.6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            
+            LinearEquation obj = new LinearEquation()
+            {
+                A=1,
+                B=4,
+                C = 0
+            };
+
+            double result = obj.F();
+            if (double.IsNaN(result))
+            {
+                Console.WriteLine($"Ошибка");
+            }
+            else
+            {
+                Console.WriteLine($"x={result}");
+            }
+        }
+    }
+}
